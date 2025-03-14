bf92633f6aee854161620530b6720320ad5d1e0e
zyx-db <dhirajbomma@gmail.com>
2025-03-14 02:22:00 -0700

    generic iterators

--- src/execution.rs
+++ src/execution.rs
@@ -1,28 +1,136 @@
-use std::collections::BTreeMap;
+use std::{any::Any, collections::BTreeMap, fmt, marker::PhantomData};
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Str,
+    U32,
+    I32,
+    Bool,
+}
 
 pub struct Row {
-    pub id: u32,
-    pub name: String,
-    pub age: u8,
+    data: Vec<Box<dyn Any>>,
 }
 
 impl Row {
-    pub fn new(id: u32, name: String, age: u8) -> Self {
-        Row { id, name, age }
+    pub fn new(data: Vec<Box<dyn Any>>) -> Self {
+        Row { data }
+    }
+
+    pub fn get<T: 'static>(&self, index: usize) -> Option<&T> 
+    {
+        self.data.get(index).and_then(|v| v.downcast_ref::<T>())
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &dyn Any> {
+        self.data.iter().map(|v| &**v)
     }
 }
 
-pub struct Scan<'a> {
-    iterator: std::collections::btree_map::Iter<'a, u32, Row>,
+#[derive(Clone)]
+pub struct RowSchema {
+    source_iter: Vec<usize>,
+    iter_index: Vec<usize>,
+    data_types: Vec<Type>
 }
 
-impl<'a> Scan<'a> {
-    pub fn new(map: &'a BTreeMap<u32, Row>) -> Self {
-        Scan { iterator: map.iter() }
+impl RowSchema {
+    pub fn new(source_iter: Vec<usize>, iter_index: Vec<usize>, data_types: Vec<Type>) -> Self {
+        RowSchema { source_iter, iter_index, data_types }
+    }
+
+    pub fn generate_from_rows(&self, rows: Vec<&Row>) -> Row{
+        let mut data: Vec<Box<dyn Any>> = Vec::new();
+        for i in 0..self.source_iter.len(){
+            let source_idx = self.source_iter[i];
+            let idx = self.iter_index[i];
+            let t = &self.data_types[i];
+
+            let r = rows[source_idx];
+
+            use Type::*;
+            match t {
+                U32 => {
+                    let x = r.get::<u32>(idx).unwrap().clone();
+                    data.push(Box::new(x));
+                }
+                I32 => {
+                    let x = r.get::<i32>(idx).unwrap().clone();
+                    data.push(Box::new(x));
+                }
+                Bool => {
+                    let x = r.get::<bool>(idx).unwrap().clone();
+                    data.push(Box::new(x));
+                }
+                Str => {
+                    let x = r.get::<String>(idx).unwrap().clone();
+                    data.push(Box::new(x));
+                }
+            }
+        }
+
+        return Row::new(data);
+    }
+
+    pub fn print(&self, r: &Row) -> String {
+        let row_len = r.len();
+        let output: String = r.iter()
+            .enumerate()
+            .map(|(i, v)| {
+                let t = &self.data_types[i];
+                let trailing = {
+                    if i < row_len - 1 {" - "}
+                    else {""}
+                };
+                use Type::*;
+                let value_str = match t{
+                    U32 => {
+                        format!("{}", v.downcast_ref::<u32>().unwrap())
+                    }
+                    I32 => {
+                        format!("{}", v.downcast_ref::<i32>().unwrap())
+                    }
+                    Bool => {
+                        format!("{}", v.downcast_ref::<bool>().unwrap())
+                    }
+                    Str => {
+                        format!("{}", v.downcast_ref::<String>().unwrap())
+                    }
+                };
+                format!("field {}: {} (type{:?}){}", i, value_str, t, trailing)
+            })
+            .collect(); 
+
+        output
     }
 }
 
-impl<'a> Iterator for Scan<'a> {
+pub struct Scan<'a, T> 
+where
+    T: Ord
+{
+    iterator: std::collections::btree_map::Iter<'a, T, Row>,
+}
+
+impl<'a, T> Scan<'a, T> 
+where
+    T: Ord
+{
+    pub fn new(map: &'a BTreeMap<T, Row>) -> Self {
+        Scan {
+            iterator: map.iter(),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Scan<'a, T> 
+where
+    T: Ord
+{
     type Item = &'a Row;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -66,35 +174,119 @@ where
     }
 }
 
-pub struct JoinIterator<'a, I>
+pub struct HashJoinIterator<'a, I, T>
 where
     I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'static,
 {
     left: I,
-    right: I
+    right: I,
+    left_key_idx: usize,
+    right_key_idx: usize,
+    phantom: PhantomData<&'a T>
 }
 
-impl<'a, I> JoinIterator<'a, I>
+impl<'a, I, T> HashJoinIterator<'a, I, T>
 where
     I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'static,
 {
-    pub fn new(left: I, right: I) -> Self {
-        JoinIterator {left, right}
+    pub fn new(left: I, right: I, left_key_idx: usize, right_key_idx: usize) -> Self {
+        HashJoinIterator::<I, T> {
+            left,
+            right,
+            left_key_idx,
+            right_key_idx,
+            phantom: PhantomData
+        }
     }
 }
 
-impl<'a, I> Iterator for JoinIterator<'a, I>
+impl<'a, I, T> Iterator for HashJoinIterator<'a, I, T>
 where
     I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'a,
 {
     type Item = &'a Row;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let left_map: BTreeMap<u32, &Row> = self
+        //let left_map: BTreeMap<T, &'a Row> = BTreeMap::new();
+        let l = self.left_key_idx;
+        let left_map: BTreeMap<T, &'a Row>= self
             .left
             .by_ref()
-            .map(|row| (row.id.clone(), row))
+            .map(|row| {
+                let key = row.get::<T>(l).unwrap().clone();
+                (key, row)
+            })
             .collect();
+        //let left_map: BTreeMap<&'a T, &'a Row>= self
+        //    .left
+        //    .by_ref()
+        //    .map(|row| {
+        //        let r = row.clone();
+        //        let key = r.get::<T>(l).unwrap().clone();
+        //        (key, r)
+        //    })
+        //    .collect();
+
+        None
+    }
+}
+
+pub struct NestedJoinIterator<'a, I, T>
+where
+    I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'static,
+{
+    left: I,
+    right: I,
+    left_key_idx: usize,
+    right_key_idx: usize,
+    output_schema: RowSchema,
+    outputs: Vec<Row>,
+    phantom: PhantomData<&'a T>,
+}
+
+impl<'a, I, T> NestedJoinIterator<'a, I, T>
+where
+    I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'static,
+{
+    pub fn new(left: I, right: I, left_key_idx: usize, right_key_idx: usize, output_schema: RowSchema) -> Self {
+        let outputs = Vec::new();
+        NestedJoinIterator::<I, T> {
+            left,
+            right,
+            left_key_idx,
+            right_key_idx,
+            output_schema,
+            outputs,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, I, T> Iterator for NestedJoinIterator<'a, I, T>
+where
+    I: Iterator<Item = &'a Row>,
+    T: Ord + Clone + 'a,
+{
+    type Item = &'a Row;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(outer_row) = self.left.next() {
+            while let Some(inner_row) = self.right.next() {
+                let key_left = outer_row.get::<T>(self.left_key_idx).unwrap();
+                let key_right = inner_row.get::<T>(self.right_key_idx).unwrap();
+                if key_left == key_right{
+                    let x = vec![outer_row, inner_row];
+                    let result = self.output_schema.generate_from_rows(x);
+                    self.outputs.push(result);
+                    //return Some(&outputs[outputs.len() - 1]);
+                }
+            }
+        }
 
         None
     }
--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,36 @@ mod execution;
 
 use execution::*;
 
-use std::collections::BTreeMap;
 use sqlparser::dialect::SQLiteDialect;
 use sqlparser::parser::Parser;
+use std::collections::BTreeMap;
+use std::any::Any;
 
 fn main() {
-    let mut data = BTreeMap::new();
-    let names_ages = vec![
-        ("Dhiraj".to_string(), 20),
-        ("db".to_string(), 6),
-        ("bomma".to_string(), 8),
-        ("test".to_string(), 40),
-        ("hello".to_string(), 10),
-        ("??".to_string(), 220),
-    ];
 
-    for (i, (n, a)) in names_ages.into_iter().enumerate() {
-        data.insert(i as u32, Row::new(i as u32, n, a));
-    }
+    let schema = RowSchema::new(
+            vec![1, 1, 1],
+            vec![0, 1, 2],
+            vec![Type::U32, Type::Str, Type::U32]
+        );
 
-    let scan = Scan::new(&data);
-    let f1 = FilterIterator::new(scan, |row| row.id % 2 == 0);
-    let filter = FilterIterator::new(f1, |row| row.age > 9);
+    let data = BTreeMap::from([
+        (1 as u32, Row::new(vec![Box::new(1 as u32), Box::new("Dhiraj".to_string()), Box::new(20 as u32)])),
+        (2 as u32, Row::new(vec![Box::new(2 as u32), Box::new("db".to_string()), Box::new(6 as u32)])),
+        (3 as u32, Row::new(vec![Box::new(3 as u32), Box::new("bomma".to_string()), Box::new(8 as u32)])),
+        (4 as u32, Row::new(vec![Box::new(4 as u32), Box::new("test".to_string()), Box::new(40 as u32)])),
+        (5 as u32, Row::new(vec![Box::new(5 as u32), Box::new("hello".to_string()), Box::new(10 as u32)])),
+        (6 as u32, Row::new(vec![Box::new(6 as u32), Box::new("??".to_string()), Box::new(220 as u32)])),
+    ]);
+
+
+    let scan: Scan<'_, u32> = Scan::new(&data);
+    let f1 = FilterIterator::new(scan, |row| row.get::<u32>(0).unwrap() % 2 == 0);
+    let filter = FilterIterator::new(f1, |row| row.get::<u32>(2).unwrap() > &9);
+
+    let output_schema = schema.clone();
 
     for row in filter {
-        println!("Filtered Row: {} - {} - {}", row.id, row.name, row.age);
+        println!("{}", output_schema.print(row));
     }
-
-    let dialect = SQLiteDialect {}; // or AnsiDialect
-
-    let sql = "SELECT * \
-               FROM generic \
-               WHERE id % 2 == 0 \
-               AND age > 9";
-
-    let ast = Parser::parse_sql(&dialect, sql).unwrap();
-
-    println!("AST: {:?}", ast);
 }
